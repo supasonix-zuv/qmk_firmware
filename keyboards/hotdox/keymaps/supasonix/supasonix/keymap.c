3780a8b95e89b5aab9a906b004a2669b1b58b255
supasonix <supasonix@cock.email>
2022-01-28 20:59:11 -0500

    changes 2

--- keyboards/hotdox/keymaps/supasonix/supasonix/keymap.c
+++ keyboards/hotdox/keymaps/supasonix/supasonix/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   LGUI(KC_LBRC),
         KC_DEL,         KC_Q,         KC_D,   KC_R,   KC_W,   KC_B,   TG(SYMB),
-        KC_BSPC,        KC_A,         KC_S,   KC_H,   KC_T,   KC_G,
+        KC_LCTL,        KC_A,         KC_S,   KC_H,   KC_T,   KC_G,
         KC_LSFT,        CTL_T(KC_Z),  KC_X,   KC_M,   KC_C,   KC_COMM,   TG(MDIA),
         LT(SYMB,KC_GRV),KC_QUOT,      LALT(KC_LSFT),  KC_LBRC,KC_RBRC,
                                               KC_HOME,  KC_END,
